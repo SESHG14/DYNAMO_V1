5652f9bdfa4a96115291973a5bec503a68e9a54a
SESH_G14 <sgovender1402@gmail.com>
2023-07-05 18:26:06 +0200

    Latest commit

--- app/src/main/java/com/example/carone_prototype_1/Controller.java
+++ app/src/main/java/com/example/carone_prototype_1/Controller.java
@@ -2,9 +2,13 @@ package com.example.carone_prototype_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,10 +18,96 @@ import org.w3c.dom.Text;
 
 public class Controller extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+
+        float throttleOrigin, turnOrigin;
+        final float[] x = new float[1];
+        final float[] y = new float[1];
+
+        final float[] throttleMagnitude = new float[1];
+        final float[] turnMagnitude = new float[1];
+
+        final String[] throttleDirection = new String[1];
+        final String[] TurnDirection = new String[1];
+        FrameLayout leftStick = findViewById(R.id.flLeftStick);
+        //throttleOrigin = leftStick.getY()/2 - leftStick.getHeight()/2;
+        throttleOrigin = leftStick.getY() - leftStick.getHeight() + 310; //had to include an offset factor. Its a rough estimate but works fine.
+        //==================== Left Stick (fwrd/bckwrd) on-touch Listener ==========================
+
+        leftStick.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                //Log.i("LeftJS", "Touch detected");
+
+                int eventType = motionEvent.getActionMasked();
+
+                switch (eventType){
+
+                    case MotionEvent.ACTION_DOWN:
+                        Log.i("LeftJS", "Action Down");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        throttleMagnitude[0] = 0;
+                        Log.i("LeftJS", "Not Moving " + " == Magnitude: " + throttleMagnitude[0]);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        y[0] = motionEvent.getY();
+                        throttleMagnitude[0] = throttleOrigin - y[0];
+                        if (throttleMagnitude[0]<0){
+                            throttleDirection[0] = "Reverse";
+                        }
+                        else{
+                            throttleDirection[0] = "Forward";
+                        }
+                        Log.i("LeftJS", "Moving " + throttleDirection[0] + " == Magnitude: " + throttleMagnitude[0]);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+        FrameLayout rightStick = findViewById(R.id.flRightStick);
+        turnOrigin = rightStick.getX() - rightStick.getWidth()+310;
+        //==================== Right Stick (left/right) on-touch Listener ==========================
+
+        rightStick.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                //Log.i("RightJS", "Touch detected");
+                int eventType = motionEvent.getActionMasked();
+
+                switch (eventType){
+
+                    case MotionEvent.ACTION_DOWN:
+                        Log.i("RightJS", "Action Down");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        turnMagnitude[0] = 0;
+                        Log.i("RightJS", "Not Turning " + " == Magnitude: " + turnMagnitude[0]);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        x[0] = motionEvent.getX();
+                        turnMagnitude[0] = turnOrigin - x[0];
+                        if (turnMagnitude[0]<0){
+                            TurnDirection[0] = "Right";
+                        }
+                        else{
+                            TurnDirection[0] = "Left";
+                        }
+                        Log.i("RightJS", "Turning " + TurnDirection[0] + " == Magnitude: " + turnMagnitude[0]);
+                        break;
+
+                }
+                return true;
+            }
+        });
 
         ImageButton home  = findViewById(R.id.btnHome); //Object for home button
 
